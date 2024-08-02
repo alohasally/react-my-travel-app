3f7fe976782805ab7e8501d92d227741a8d612c7
alohasally <peacheeks@gmail.com>
2024-08-02 23:00:41 +0900

    homepage edit

--- final/src/pages/Homepage.jsx
+++ final/src/pages/Homepage.jsx
@@ -8,15 +8,17 @@ export default function Homepage() {
       <PageNav />
 
       <section>
-        <h1>
-          You travel the world.
-          <br />
-          WorldWise keeps track of your adventures.
-        </h1>
+        <div>
+          <h2>You travel the world.</h2>
+          <h1>
+            Keep track of your adventures <br />
+            on My Travel List
+          </h1>
+        </div>
         <h2>
           A world map that tracks your footsteps into every city you can think
-          of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
+          of. <br /> Never forget your wonderful experiences, <br />
+          and show your friends how you have wandered the world.
         </h2>
         <Link to="/login" className="cta">
           Start tracking now
